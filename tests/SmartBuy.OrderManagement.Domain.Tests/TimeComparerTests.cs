09d696ba7aae696a49c1d26cbe9c2db0f01b3f88
Raja Kondla <rajakondla@gmail.com>
2020-09-08 15:19:00 +0000

    deleted time comparer test file

--- tests/SmartBuy.OrderManagement.Domain.Tests/TimeComparerTests.cs
+++ tests/SmartBuy.OrderManagement.Domain.Tests/TimeComparerTests.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SmartBuy.OrderManagement.Domain.Tests.Helper
+namespace SmartBuy.OrderManagement.Domain.Tests
 {
     class TimeComparerTests
     {
