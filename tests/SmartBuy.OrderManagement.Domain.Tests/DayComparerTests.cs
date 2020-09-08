315cbe71cb0819381941894a99d4543e836f6445
Raja Kondla <rajakondla@gmail.com>
2020-09-08 15:17:43 +0000

    added day comparer tests

--- tests/SmartBuy.OrderManagement.Domain.Tests/DayComparerTests.cs
+++ tests/SmartBuy.OrderManagement.Domain.Tests/DayComparerTests.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Moq;
+using SmartBuy.OrderManagement.Domain.Services.ScheduleOrderGenerator;
+using Xunit;
+using System;
 
 namespace SmartBuy.OrderManagement.Domain.Tests
 {
-    class DayComparerTests
+    public class DayComparerTests
     {
+        [Fact]
+        public void ShouldReturnTrueWhenDayIsToday()
+        {
+            var dayCompare = new DayComparer();
+
+            Assert.True(dayCompare.Compare(DateTime.UtcNow.DayOfWeek));
+        }
+
+        [Fact]
+        public void ShouldReturnFalseWhenDayIsNotToday()
+        {
+            var dayCompare = new DayComparer();
+
+            Assert.False(dayCompare.Compare(DateTime.UtcNow.AddDays(-1).DayOfWeek));
+        }
     }
 }
---
+++ tests/SmartBuy.OrderManagement.Domain.Tests/Helper/TimeComparerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartBuy.OrderManagement.Domain.Tests.Helper
+{
+    class TimeComparerTests
+    {
+    }
+}
